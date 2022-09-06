9ca1e8c49dc2eb0ce917ab953a322b65947c46ca
demog <dg08gg@mail.ru>
2022-09-06 20:53:38 +0300

    Роутер страниц

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import NavBar from "./components/UI/Navbar/NavBar";
 import Home from "./utils/Home";
 import Add from "./utils/Add";
-import Contacts from "./utils/Contacts";
 import About from "./utils/About";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import CardsIdPage from "./utils/CardsIdPage";
+import Contacts from "./utils/Contacts";
 
 function App() {
   return(
@@ -15,6 +16,8 @@ function App() {
         <Route index element={<Home/>} />
         <Route path='/add' element={<Add/>} />
         <Route path='/contacts' element={<Contacts/>} />
+        <Route path='/contacts/:id' element={<CardsIdPage />} />
+        <Route path='/add/:id' element={<Add />} />
         <Route path='/home' element={<Home/>} />
         <Route path='/about' element={<About/>} />
         <Route path='*' element={<Home/>} />
